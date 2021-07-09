97725277622e8bb2ab7b37ea32bd037fd28adbf5
pedro <pedro.donoso.r@usach.cl>
2021-07-09 13:11:18 -0400

    agrego while
    creo ciclo for

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,17 @@
         if(isNaN(miNumero) || miNumero < 1 || miNumero > 20){
             alert("Tú N° ingresado está fuera del rango (1 al 20); vuelve a ingresar");
         }
-        
+        while (isNaN(miNumero) || miNumero < 1 || miNumero > 20);
+        let auxiliar = '';
+        for (let index = 1; index <= miNumero; index++) {
+            console.log('${index} x ${numero} = ${numero * index}');
+            let acumulacion = 1;
+            for (let i = 1; i <= index; i++){
+                acumulacion *= i;
+            }
+            aux += 'El resultado Factorial de ${index} es: ${acumulador}\n'
+        }
+
+        console.log(auxiliar);
 
     }
