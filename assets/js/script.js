cefb48f7ac51dbc52ef7d8f8b293d900ed3b9e0a
pedro <pedro.donoso.r@usach.cl>
2021-07-09 13:39:51 -0400

    agrego salto de linea
    fin desafio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const inicio = () => {
     let minumero;
     do {
-        minumero = parseInt(prompt("ingresa tu número desde 1 a 20, el resultado se mostrará por CONSOLA"));
+        minumero = parseInt(prompt("ingresa tu número desde 1 a 20, \n el resultado se mostrará por CONSOLA"));
         if (isNaN(minumero) || minumero < 1 || minumero > 20) {
             alert("El N° que ingresaste está fuera de rango (1 - 20); repite el ingreso.");
           
